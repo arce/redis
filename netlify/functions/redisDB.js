16955f402a1181401dd3c3ce6b44b7cbeb1fcfb2
Armando Arce <armando.arce@gmail.com>
2022-11-22 12:27:27 -0600

    commit

--- netlify/functions/redisDB.js
+++ netlify/functions/redisDB.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { Redis } = require('ioredis');
+const Redis = require('ioredis');
 
 const client = new Redis({host:process.env.REDIS_HOST, port:19183,
                          password:process.env.REDIS_PSW});
