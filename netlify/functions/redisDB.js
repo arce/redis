dbb622117fc159be1912c154d409bf1ed34ddc34
Armando Arce <armando.arce@gmail.com>
2022-11-22 12:16:12 -0600

    update

--- netlify/functions/redisDB.js
+++ netlify/functions/redisDB.js
@@ -2,7 +2,7 @@
 
 const { Redis } = require('ioredis');
 
-const client = new Redis(host:process.env.REDIS_HOST, port:19183,
-                         password:process.env.REDIS_PSW);
+const client = new Redis({host:process.env.REDIS_HOST, port:19183,
+                         password:process.env.REDIS_PSW});
 
 module.exports = client.connect();
