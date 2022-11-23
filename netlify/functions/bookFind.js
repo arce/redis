06a419bd4ff64b4758231d5301b3411c5794c834
Armando Arce <armando.arce@gmail.com>
2022-11-22 20:21:18 -0600

    Update bookFind.js

--- netlify/functions/bookFind.js
+++ netlify/functions/bookFind.js
@@ -1,18 +1,34 @@
 "use strict"
+
 const redis = require('./redisDB');
 const headers = require('./headersCORS');
 
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+
 exports.handler = async (event, context) => {
+
   if (event.httpMethod == "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
-  
+
   try {
-    const client = await clientPromise;
-    const id = parseInt(event.path.split("/").reverse()[0]);
-    const authors = 
-      await client.db("bookstore").collection("books").find({_id:id}).toArray();
-    return { statusCode: 200, headers, body: JSON.stringify(authors)};
+    
+    const id = event.path.split("/").reverse()[0];
+    
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+
+   const book = await redis.get(id);
+    
+	 let books = [];
+ 
+   books.push(book);
+   books.forEach(toJson);
+		
+    return { statusCode: 200, headers, body: JSON.stringify(books)};
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
