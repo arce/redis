b2b65e5846d9a6b69609454824c5be5cbcb38dab
Armando Arce <armando.arce@gmail.com>
2022-11-22 19:50:01 -0600

    Update app.js

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
 
    const books = await redis.mget(keys);
 		
-    return { statusCode: 200, headers, body: books};
+    return { statusCode: 200, headers, body: JSON.stringify(books)};
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
